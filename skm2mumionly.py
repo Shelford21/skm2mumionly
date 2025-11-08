374cda12ce6e3d69e9f159ca4520c0471bf75f26
Fauzan Fadhillah Arisandi <122199835+Shelford21@users.noreply.github.com>
2025-11-09 00:19:29 +0700

    Update skm2mumionly.py

--- skm2mumionly.py
+++ skm2mumionly.py
@@ -51,6 +51,11 @@ st.markdown(
         unsafe_allow_html=True
     )
 
+now = datetime.now()
+# Format: 2025-11-08 19:42:10 (Year-Month-Day Hour:Minute:Second)
+formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
+
+st.markdown(f"### 📅 Waktu saat ini: {formatted_now}")
 
 # Safely slice rows B6:B27 (column index 1 since A=0, B=1)
 name_list = name.iloc[5:27, 1].dropna().astype(str).tolist()  # B6:B27
@@ -251,6 +256,7 @@ if admin_password == ADMIN_PASSWORD:
 else:
     if admin_password != "":
         st.error("❌ Incorrect password.")
+
 
 
 
