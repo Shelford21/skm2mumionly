0c2b6e85d487eb61cce094d03d442a6daa08a41e
Fauzan Fadhillah Arisandi <122199835+Shelford21@users.noreply.github.com>
2025-11-08 23:48:32 +0700

    Update skm2mumionly.py

--- skm2mumionly.py
+++ skm2mumionly.py
@@ -118,27 +118,30 @@ if st.button("Submit Kehadiran"):
     # if user_input.strip() == "":
     #     st.warning("Tidak boleh kosong ok!")
     #else:
-    if name_row:
-        row_idx = name_row[0]
-         # Column D=3 (0-based index), so date 1 = col 3
-        col_idx = 3 + (selected_date - 1)
-
-        if selected_status == "-":
-            st.warning("Isi hadir/ijin/sakit ok")
-        else:
-            name.iat[row_idx, col_idx] = status_map[selected_status]
-        
-             # Update Google Sheet
-            conn.update(worksheet=url, data=name)
-            #st.success(f"✅ Kehadiran {selected_name} untuk tanggal {selected_date} tersimpan sebagai '{status_map[selected_status]}'")
-            if selected_status == "Hadir":
-                st.success(f"✅ جَزَاكُمُ اللهُ خَيْرًا {selected_name} - Semoga kehadiran hari ini dapat memberikan kebarokahan dan ilmu yang bermanfaat")
-            elif selected_status == "Ijin":
-                st.success(f"✅ جَزَاكُمُ اللهُ خَيْرًا {selected_name} - Semoga allah paring banyak waktu longgar sehingga dapat hadir dijadwal sambung selanjutnya")
-            elif selected_status == "Sakit":
-                st.success(f"✅ جَزَاكُمُ اللهُ خَيْرًا {selected_name} - Semoga allah paring kesembuhan dan kesehatan yang barokah sehingga dapat hadir dijadwal sambung selanjutnya")
+    if selected_name == "-":
+        st.warning("⚠️ Silakan pilih nama terlebih dahulu.")
     else:
-         st.error("Nama tidak ditemukan dalam daftar.")
+        if name_row:
+            row_idx = name_row[0]
+             # Column D=3 (0-based index), so date 1 = col 3
+            col_idx = 3 + (selected_date - 1)
+    
+            if selected_status == "-":
+                st.warning("Isi hadir/ijin/sakit ok")
+            else:
+                name.iat[row_idx, col_idx] = status_map[selected_status]
+            
+                 # Update Google Sheet
+                conn.update(worksheet=url, data=name)
+                #st.success(f"✅ Kehadiran {selected_name} untuk tanggal {selected_date} tersimpan sebagai '{status_map[selected_status]}'")
+                if selected_status == "Hadir":
+                    st.success(f"✅ جَزَاكُمُ اللهُ خَيْرًا {selected_name} - Semoga kehadiran hari ini dapat memberikan kebarokahan dan ilmu yang bermanfaat")
+                elif selected_status == "Ijin":
+                    st.success(f"✅ جَزَاكُمُ اللهُ خَيْرًا {selected_name} - Semoga allah paring banyak waktu longgar sehingga dapat hadir dijadwal sambung selanjutnya")
+                elif selected_status == "Sakit":
+                    st.success(f"✅ جَزَاكُمُ اللهُ خَيْرًا {selected_name} - Semoga allah paring kesembuhan dan kesehatan yang barokah sehingga dapat hadir dijadwal sambung selanjutnya")
+        else:
+             st.error("Nama tidak ditemukan dalam daftar.")
             
 if os.path.exists(CSV_FILE):
     st.subheader("Kehadiran hari ini:")
@@ -230,6 +233,7 @@ if admin_password == ADMIN_PASSWORD:
 else:
     if admin_password != "":
         st.error("❌ Incorrect password.")
+
 
 
 
