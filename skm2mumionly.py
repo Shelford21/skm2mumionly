cc9245ade535244f80c230a570868becd5a44659
Fauzan Fadhillah Arisandi <122199835+Shelford21@users.noreply.github.com>
2025-11-09 11:33:03 +0700

    Update skm2mumionly.py

--- skm2mumionly.py
+++ skm2mumionly.py
@@ -17,15 +17,17 @@ load_css()
 url = "AbsenNovember2025"
 urlp = "percobaan"
 
-percobaan= conn.read(worksheet=urlp)
-value_b7 = url.iat[6, 1] 
-urlp.iat[6, 9] = f"Komentar otomatis: {value_b7}"
-conn.update(worksheet=urlp, data=urlp)
+
 #urll = "https://docs.google.com/spreadsheets/d/1dK2tKeeRGAiVc6p0guapTITane-NckvuAFB3rrHu3k8/edit?usp=sharing"
 # sheet_id = "1dK2tKeeRGAiVc6p0guapTITane-NckvuAFB3rrHu3k8"
 # excel_link = f"https://docs.google.com/spreadsheets/d/1dK2tKeeRGAiVc6p0guapTITane-NckvuAFB3rrHu3k8/export?format=xlsx"
 
 conn = st.connection("gsheets", type=GSheetsConnection)
+
+percobaan= conn.read(worksheet=urlp)
+value_b7 = url.iat[6, 1] 
+urlp.iat[6, 9] = f"Komentar otomatis: {value_b7}"
+conn.update(worksheet=urlp, data=urlp)
 
 #data = conn.read(spreadsheet=url, worksheet="1750077145")
 data = conn.read(worksheet=url)
@@ -281,6 +283,7 @@ if admin_password == ADMIN_PASSWORD:
 else:
     if admin_password != "":
         st.error("❌ Incorrect password.")
+
 
 
 
